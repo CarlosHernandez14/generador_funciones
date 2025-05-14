e4e53f421ac0f4a2980a19542e91bb119843760e
CarlosHernandez14 <carloskvpchc@gmail.com>
2025-05-13 23:20:45 -0600

    Modify design

--- GeneradorFunciones/Form1.Designer.cs
+++ GeneradorFunciones/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.signalsChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.trackBarFrequency = new System.Windows.Forms.TrackBar();
             this.lblFrequency = new System.Windows.Forms.Label();
@@ -63,19 +63,20 @@
             // 
             // signalsChart
             // 
-            chartArea3.Name = "ChartArea1";
-            this.signalsChart.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.signalsChart.Legends.Add(legend3);
-            this.signalsChart.Location = new System.Drawing.Point(1, -1);
+            chartArea1.Name = "ChartArea1";
+            this.signalsChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.signalsChart.Legends.Add(legend1);
+            this.signalsChart.Location = new System.Drawing.Point(-3, 359);
             this.signalsChart.Name = "signalsChart";
-            series3.BorderWidth = 3;
-            series3.ChartArea = "ChartArea1";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series3.Legend = "Legend1";
-            series3.Name = "Signal";
-            this.signalsChart.Series.Add(series3);
-            this.signalsChart.Size = new System.Drawing.Size(507, 510);
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Purple;
+            series1.Legend = "Legend1";
+            series1.Name = "Signal";
+            this.signalsChart.Series.Add(series1);
+            this.signalsChart.Size = new System.Drawing.Size(928, 308);
             this.signalsChart.TabIndex = 0;
             this.signalsChart.Text = "chart1";
             // 
@@ -92,6 +93,7 @@
             // 
             this.lblFrequency.AutoSize = true;
             this.lblFrequency.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrequency.ForeColor = System.Drawing.Color.Purple;
             this.lblFrequency.Location = new System.Drawing.Point(527, 180);
             this.lblFrequency.Name = "lblFrequency";
             this.lblFrequency.Size = new System.Drawing.Size(124, 20);
@@ -102,6 +104,7 @@
             // 
             this.lblAmplitude.AutoSize = true;
             this.lblAmplitude.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmplitude.ForeColor = System.Drawing.Color.Purple;
             this.lblAmplitude.Location = new System.Drawing.Point(527, 259);
             this.lblAmplitude.Name = "lblAmplitude";
             this.lblAmplitude.Size = new System.Drawing.Size(98, 20);
@@ -120,7 +123,8 @@
             // 
             this.lblOffset.AutoSize = true;
             this.lblOffset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOffset.Location = new System.Drawing.Point(527, 343);
+            this.lblOffset.ForeColor = System.Drawing.Color.Purple;
+            this.lblOffset.Location = new System.Drawing.Point(169, 174);
             this.lblOffset.Name = "lblOffset";
             this.lblOffset.Size = new System.Drawing.Size(78, 20);
             this.lblOffset.TabIndex = 6;
@@ -128,7 +132,7 @@
             // 
             // trackBarOffset
             // 
-            this.trackBarOffset.Location = new System.Drawing.Point(531, 376);
+            this.trackBarOffset.Location = new System.Drawing.Point(173, 207);
             this.trackBarOffset.Name = "trackBarOffset";
             this.trackBarOffset.Size = new System.Drawing.Size(294, 45);
             this.trackBarOffset.TabIndex = 5;
@@ -138,7 +142,8 @@
             // 
             this.lblSimetry.AutoSize = true;
             this.lblSimetry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSimetry.Location = new System.Drawing.Point(527, 417);
+            this.lblSimetry.ForeColor = System.Drawing.Color.Purple;
+            this.lblSimetry.Location = new System.Drawing.Point(169, 248);
             this.lblSimetry.Name = "lblSimetry";
             this.lblSimetry.Size = new System.Drawing.Size(188, 20);
             this.lblSimetry.TabIndex = 8;
@@ -146,7 +151,7 @@
             // 
             // trackBarSimetry
             // 
-            this.trackBarSimetry.Location = new System.Drawing.Point(531, 450);
+            this.trackBarSimetry.Location = new System.Drawing.Point(173, 281);
             this.trackBarSimetry.Name = "trackBarSimetry";
             this.trackBarSimetry.Size = new System.Drawing.Size(294, 45);
             this.trackBarSimetry.TabIndex = 7;
@@ -154,75 +159,85 @@
             // 
             // btnSquareSignal
             // 
+            this.btnSquareSignal.BackColor = System.Drawing.SystemColors.Highlight;
             this.btnSquareSignal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSquareSignal.Location = new System.Drawing.Point(531, 69);
+            this.btnSquareSignal.ForeColor = System.Drawing.Color.White;
+            this.btnSquareSignal.Location = new System.Drawing.Point(12, 213);
             this.btnSquareSignal.Name = "btnSquareSignal";
             this.btnSquareSignal.Size = new System.Drawing.Size(120, 23);
             this.btnSquareSignal.TabIndex = 9;
             this.btnSquareSignal.Text = "CUADRADA";
-            this.btnSquareSignal.UseVisualStyleBackColor = true;
+            this.btnSquareSignal.UseVisualStyleBackColor = false;
             this.btnSquareSignal.Click += new System.EventHandler(this.btnSquareSignal_Click);
             // 
             // btnSignalTriangular
             // 
+            this.btnSignalTriangular.BackColor = System.Drawing.SystemColors.Highlight;
             this.btnSignalTriangular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSignalTriangular.Location = new System.Drawing.Point(531, 98);
+            this.btnSignalTriangular.ForeColor = System.Drawing.Color.White;
+            this.btnSignalTriangular.Location = new System.Drawing.Point(12, 242);
             this.btnSignalTriangular.Name = "btnSignalTriangular";
             this.btnSignalTriangular.Size = new System.Drawing.Size(120, 23);
             this.btnSignalTriangular.TabIndex = 10;
             this.btnSignalTriangular.Text = "TRIANGULAR";
-            this.btnSignalTriangular.UseVisualStyleBackColor = true;
+            this.btnSignalTriangular.UseVisualStyleBackColor = false;
             this.btnSignalTriangular.Click += new System.EventHandler(this.btnSignalTriangular_Click);
             // 
             // btnSignalSenoidal
             // 
+            this.btnSignalSenoidal.BackColor = System.Drawing.SystemColors.Highlight;
             this.btnSignalSenoidal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSignalSenoidal.Location = new System.Drawing.Point(531, 127);
+            this.btnSignalSenoidal.ForeColor = System.Drawing.Color.White;
+            this.btnSignalSenoidal.Location = new System.Drawing.Point(12, 274);
             this.btnSignalSenoidal.Name = "btnSignalSenoidal";
             this.btnSignalSenoidal.Size = new System.Drawing.Size(120, 23);
             this.btnSignalSenoidal.TabIndex = 11;
             this.btnSignalSenoidal.Text = "SENOIDAL";
-            this.btnSignalSenoidal.UseVisualStyleBackColor = true;
+            this.btnSignalSenoidal.UseVisualStyleBackColor = false;
             this.btnSignalSenoidal.Click += new System.EventHandler(this.btnSignalSenoidal_Click);
             // 
             // btnConnectSerial
             // 
-            this.btnConnectSerial.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConnectSerial.Location = new System.Drawing.Point(695, 69);
+            this.btnConnectSerial.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnConnectSerial.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnectSerial.ForeColor = System.Drawing.Color.White;
+            this.btnConnectSerial.Location = new System.Drawing.Point(551, 24);
             this.btnConnectSerial.Name = "btnConnectSerial";
             this.btnConnectSerial.Size = new System.Drawing.Size(185, 39);
             this.btnConnectSerial.TabIndex = 12;
-            this.btnConnectSerial.Text = "Conectar Puerto Serial";
-            this.btnConnectSerial.UseVisualStyleBackColor = true;
+            this.btnConnectSerial.Text = "Conectar Serial";
+            this.btnConnectSerial.UseVisualStyleBackColor = false;
             this.btnConnectSerial.Click += new System.EventHandler(this.btnConnectSerial_Click);
             // 
             // btnDisconnectSerial
             // 
-            this.btnDisconnectSerial.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDisconnectSerial.Location = new System.Drawing.Point(695, 114);
+            this.btnDisconnectSerial.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnDisconnectSerial.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisconnectSerial.ForeColor = System.Drawing.Color.White;
+            this.btnDisconnectSerial.Location = new System.Drawing.Point(551, 82);
             this.btnDisconnectSerial.Name = "btnDisconnectSerial";
             this.btnDisconnectSerial.Size = new System.Drawing.Size(206, 36);
             this.btnDisconnectSerial.TabIndex = 13;
-            this.btnDisconnectSerial.Text = "Desconectar Puerto Serial";
-            this.btnDisconnectSerial.UseVisualStyleBackColor = true;
+            this.btnDisconnectSerial.Text = "Desconectar Serial";
+            this.btnDisconnectSerial.UseVisualStyleBackColor = false;
             this.btnDisconnectSerial.Click += new System.EventHandler(this.btnDisconnectSerial_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(693, 25);
+            this.label1.Location = new System.Drawing.Point(384, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 18);
+            this.label1.Size = new System.Drawing.Size(56, 18);
             this.label1.TabIndex = 14;
-            this.label1.Text = "Conexion Serial:";
+            this.label1.Text = "Serial:";
             // 
             // lblSerialStatus
             // 
             this.lblSerialStatus.AutoSize = true;
             this.lblSerialStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSerialStatus.ForeColor = System.Drawing.Color.Blue;
-            this.lblSerialStatus.Location = new System.Drawing.Point(693, 50);
+            this.lblSerialStatus.Location = new System.Drawing.Point(384, 82);
             this.lblSerialStatus.Name = "lblSerialStatus";
             this.lblSerialStatus.Size = new System.Drawing.Size(108, 16);
             this.lblSerialStatus.TabIndex = 15;
@@ -232,7 +247,7 @@
             // 
             this.btnChangeColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnChangeColor.ForeColor = System.Drawing.Color.Teal;
-            this.btnChangeColor.Location = new System.Drawing.Point(531, 25);
+            this.btnChangeColor.Location = new System.Drawing.Point(12, 176);
             this.btnChangeColor.Name = "btnChangeColor";
             this.btnChangeColor.Size = new System.Drawing.Size(120, 30);
             this.btnChangeColor.TabIndex = 16;
@@ -242,7 +257,7 @@
             // 
             // buttonHertz
             // 
-            this.buttonHertz.Location = new System.Drawing.Point(826, 180);
+            this.buttonHertz.Location = new System.Drawing.Point(365, 330);
             this.buttonHertz.Name = "buttonHertz";
             this.buttonHertz.Size = new System.Drawing.Size(75, 23);
             this.buttonHertz.TabIndex = 17;
@@ -252,7 +267,7 @@
             // 
             // btnKiloHertz
             // 
-            this.btnKiloHertz.Location = new System.Drawing.Point(826, 209);
+            this.btnKiloHertz.Location = new System.Drawing.Point(463, 330);
             this.btnKiloHertz.Name = "btnKiloHertz";
             this.btnKiloHertz.Size = new System.Drawing.Size(75, 23);
             this.btnKiloHertz.TabIndex = 18;
@@ -262,7 +277,7 @@
             // 
             // btnMegaHertz
             // 
-            this.btnMegaHertz.Location = new System.Drawing.Point(826, 238);
+            this.btnMegaHertz.Location = new System.Drawing.Point(562, 330);
             this.btnMegaHertz.Name = "btnMegaHertz";
             this.btnMegaHertz.Size = new System.Drawing.Size(75, 23);
             this.btnMegaHertz.TabIndex = 19;
@@ -279,7 +294,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 508);
+            this.ClientSize = new System.Drawing.Size(925, 662);
             this.Controls.Add(this.btnMegaHertz);
             this.Controls.Add(this.btnKiloHertz);
             this.Controls.Add(this.buttonHertz);
